394b56badba9798d92e9079314c9a2562606fcaa
lija-esela <116581021+lija-esela@users.noreply.github.com>
2022-10-24 15:58:21 -0500

    minor fixes
    - deletion of unused assets
    - touch-ups to CSS
    - corrections to html
    - removal of some js egg comments

--- blog/index.js
+++ blog/index.js
@@ -5,10 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (elem !== null) {
             const eggs = [
                 elem.innerText,
-                ">blogger >gigachad.jpeg",
                 "this subtitle changes",
                 "it's called we do a little blogging",
-                "smash that like button",
                 "read about my life",
                 "morshu.gif",
                 "thank you for enabling JavaScript!"
